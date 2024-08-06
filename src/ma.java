983da9af1cf62a4e8b14a0100e0ddc43e8adefb1
ABDULLAH JAVED <jatt66016@gmail.com>
2024-08-06 22:49:22 +0500

    recursive more problems

--- src/ma.java
+++ src/ma.java
@@ -1,7 +1,39 @@
 public class ma {
     public static void main(String[] args) {
+// recurssion
+        // a function call itself
+        // 5 4 3 2 1 solution recurssive code
+        // debug through stack and recurssive tree
+//printSeries(5);
+        int res=power(10, 3);
+        System.out.println(res);
 
+    }
+
+    // public static void printSeries(int num){
+//
+//     if (num>1)
+//         printSeries(num - 1);
+//     System.out.println(num);
+//// when function end it also returns automatically
+//
+//
+// }
+    // recursive
+// power calculate
+// base exp
+//
+    //n^p=  n * n^p-1
+    // power finction using recurssion
+    public static int power(int base, int exponent) {
+        if (exponent > 0)// base condition
+            return base * power(base, exponent - 1);
+             return 1;
+
+    }
 
 
 }
-}
+
+
+
